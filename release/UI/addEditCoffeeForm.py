cf1bf13c0c4f2f54c6d24573942f9222e8fa7f3c
tsemkaloalena <alenatsemkalo@gmail.com>
2019-11-24 19:11:31 +0300

    First commit

---
+++ release/UI/addEditCoffeeForm.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'addEditCoffeeForm.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class The_Other_Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(1324, 657)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        Form.setFont(font)
+        Form.setStyleSheet("color: rgb(125, 83, 58);")
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.tableWidget = QtWidgets.QTableWidget(Form)
+        font = QtGui.QFont()
+        font.setBold(False)
+        font.setWeight(50)
+        self.tableWidget.setFont(font)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.gridLayout.addWidget(self.tableWidget, 2, 0, 1, 1)
+        self.add_btn = QtWidgets.QPushButton(Form)
+        self.add_btn.setObjectName("add_btn")
+        self.gridLayout.addWidget(self.add_btn, 4, 0, 1, 1)
+        self.label = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.save_btn = QtWidgets.QPushButton(Form)
+        self.save_btn.setObjectName("save_btn")
+        self.gridLayout.addWidget(self.save_btn, 3, 0, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.add_btn.setText(_translate("Form", "Добавить строчку"))
+        self.label.setText(_translate("Form", "Кофе"))
+        self.save_btn.setText(_translate("Form", "Сохранить изменения"))
---
+++ release/main.py
@@ -0,0 +1,106 @@
+import sys
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem
+from release.UI.addEditCoffeeForm import The_Other_Ui_Form
+from release.UI.mainform import Ui_Form
+
+
+class MyWidget(QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.db = 'release/data/coffee.db'
+        self.loadUi()
+        self.btn.clicked.connect(self.change_table)
+
+    def loadUi(self):
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
+        result = cur.execute("SELECT * FROM about").fetchall()
+        title = ['ID', 'Название сорта', 'Степень обжарки', 'Молотый/в зернах', 'Описание вкуса', 'Цена',
+                 'Объём упаковки']
+
+        self.tableWidget.setColumnCount(len(title))
+        self.tableWidget.setHorizontalHeaderLabels(title)
+
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(result):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+
+        self.tableWidget.resizeColumnsToContents()
+
+    def change_table(self):
+        self.change_form = addEditCoffeeForm(self, self.db)
+        self.change_form.show()
+        # self.loadUi()
+
+
+class addEditCoffeeForm(QWidget, The_Other_Ui_Form):
+    def __init__(self, *db):
+        super().__init__()
+        self.setupUi(self)
+        self.db = db[-1]
+        self.loadUi()
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.save_btn.clicked.connect(self.save_table)
+        self.add_btn.clicked.connect(self.add)
+        self.modified = {}
+        self.new = False
+
+    def loadUi(self):
+        self.con = sqlite3.connect(self.db)
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM about").fetchall()
+        self.titles = ['ID', 'Название сорта', 'Степень обжарки', 'Молотый/в зернах', 'Описание вкуса', 'Цена',
+                       'Объём упаковки']
+
+        self.tableWidget.setColumnCount(len(self.titles))
+        self.tableWidget.setHorizontalHeaderLabels(self.titles)
+
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(result):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.tableWidget.resizeColumnsToContents()
+        self.modified = {}
+
+    def item_changed(self, item):
+        id = self.tableWidget.item(item.row(), 0).text()
+        self.modified[(self.titles[item.column()], id)] = item.text()
+
+    def save_table(self):
+        if self.modified:
+            if not self.new:
+                cur = self.con.cursor()
+                for key in self.modified.keys():
+                    cur.execute(
+                        "UPDATE about SET\n [{}]='{}' WHERE id = {}\n".format(key[0], self.modified.get(key), key[1]))
+                self.con.commit()
+            else:
+                vals = []
+                vals.append(self.tableWidget.item(self.tableWidget.rowCount() - 1, 0).text())
+                vals.append(self.tableWidget.item(self.tableWidget.rowCount() - 1, 1).text())
+                vals.append(self.tableWidget.item(self.tableWidget.rowCount() - 1, 2).text())
+                vals.append(self.tableWidget.item(self.tableWidget.rowCount() - 1, 3).text())
+                vals.append(self.tableWidget.item(self.tableWidget.rowCount() - 1, 4).text())
+                vals.append(self.tableWidget.item(self.tableWidget.rowCount() - 1, 5).text())
+                vals.append(self.tableWidget.item(self.tableWidget.rowCount() - 1, 6).text())
+                cur = self.con.cursor()
+                cur.execute(
+                    "INSERT INTO about{} VALUES{}".format(tuple(self.titles), tuple(vals)))
+                self.con.commit()
+                self.new = False
+
+    def add(self):
+        self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+        self.new = True
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
